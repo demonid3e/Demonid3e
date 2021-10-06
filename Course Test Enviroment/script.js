94ff196d6b8dd5be745cb63846388be4de466ec2
demonid3e <71412024+demonid3e@users.noreply.github.com>
2021-10-07 00:19:39 +0100

    Evenet listeners for mobile devices added to compendium

--- Course Test Enviroment/script.js
+++ Course Test Enviroment/script.js
@@ -1,49 +1,35 @@
 "use strict";
+//////////////////////////////////////////
+////////Events on Mobile Devices /////////
+//////////////////////////////////////////
 
-// DOM NAVIGATION \\
+// touchstart = when you touch the element first time
+// touchmove = will trigger all the time while finger is moving
+// touchend = when finger lifts the screen
+// touchenter = enters the element
+// touchleave = when finger leaves the element
+// touchcancel = when finger dissapears from scree
 
-//console.log(document.body);
-//console.log(document.documentElement);
-// shows child elements body
-//console.log(document.body.childNodes); //NodeList(4) [text, div.wrapper, text, script]
-//console.log(document.body.firstChild);
-//console.log(document.body.lastChild);
+// DOMContentLoaded is needed to ensure that DOM tree is buidt
 
-//console.log(document.querySelector("#current").parentNode.parentNode);
+// (e) in eventListener("touch****") have addtitional properties
+// e.touches = shows how many fingers are currently used on all screen
+// e.targetTouches = shows how many finders used in THIS element
+// e.changedTouches = how many fingers are changed (had 3 on, 1 moved out of screen = 2)
 
-// you can add data note to any html element and
-// use with querySelector to have reference to it
-// <li data-current="3">3</li>
-//
-// <li>4</li>
-
-// will get sibling but it can be empty "text node like <br>"
-// console.log(document.querySelector(`[data-current="3"]`).previousSibling);
-// console.log(document.querySelector(`[data-current="3"]`).nextSibling);
-
-// to get next ELEMENT
-// console.log(document.querySelector(`[data-current="3"]`).nextElementSibling);
-// will show you  <li>4</li>
-// console.log(document.querySelector(`[data-current="3"]`).parentElement);
-// will show you <div class="first"
-// you can use Element with all childs
-
-// each node in child.nodes
-for (let node of document.body.childNodes) {
-  console.log(node);
-  // will show you
-
-  // #text
-  // div.wrapper
-  // #text
-  // script
-
-  // if nodeName of a element will be #text
-  // using continue you will skip this iteration
-  if (node.nodeName == "#text") {
-    continue;
-  }
-  // will show only
-  // div.wrapper
-  // script
-}
+window.addEventListener("DOMContentLoaded", () => {
+  const box = document.querySelector(".box");
+  box.addEventListener("touchstart", (e) => {
+    e.preventDefault();
+    console.log("Start");
+    console.log(e.targetTouches);
+  });
+  box.addEventListener("touchmove", (e) => {
+    e.preventDefault();
+    console.log("Moving");
+  });
+  box.addEventListener("touchend", (e) => {
+    e.preventDefault();
+    console.log("ENd");
+  });
+});
--- Learning Course Demon/script/compendium.js
+++ Learning Course Demon/script/compendium.js
@@ -1,5 +1,40 @@
 "use strict";
 
+//////////////////////////////////////////
+////////Events on Mobile Devices /////////
+//////////////////////////////////////////
+
+// touchstart = when you touch the element first time
+// touchmove = will trigger all the time while finger is moving
+// touchend = when finger lifts the screen
+// touchenter = enters the element
+// touchleave = when finger leaves the element
+// touchcancel = when finger dissapears from scree
+
+// DOMContentLoaded is needed to ensure that DOM tree is buidt
+
+// (e) in eventListener("touch****") have addtitional properties
+// e.touches = shows how many fingers are currently used on all screen
+// e.targetTouches = shows how many finders used in THIS element
+// e.changedTouches = how many fingers are changed (had 3 on, 1 moved out of screen = 2)
+
+window.addEventListener("DOMContentLoaded", () => {
+  const box = document.querySelector(".box");
+  box.addEventListener("touchstart", (e) => {
+    e.preventDefault();
+    console.log("Start");
+    console.log(e.targetTouches);
+  });
+  box.addEventListener("touchmove", (e) => {
+    e.preventDefault();
+    console.log("Moving");
+  });
+  box.addEventListener("touchend", (e) => {
+    e.preventDefault();
+    console.log("ENd");
+  });
+});
+
 //////////////////////////////
 //////// DOM NAVIGATION //////
 //////////////////////////////
