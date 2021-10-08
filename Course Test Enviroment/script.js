52c7c2440577f035a23c68304b13741b95e3749b
demonid3e <71412024+demonid3e@users.noreply.github.com>
2021-10-08 23:31:53 +0100

    new course info script async loading

--- Course Test Enviroment/script.js
+++ Course Test Enviroment/script.js
@@ -1,35 +1,37 @@
 "use strict";
-//////////////////////////////////////////
-////////Events on Mobile Devices /////////
-//////////////////////////////////////////
 
-// touchstart = when you touch the element first time
-// touchmove = will trigger all the time while finger is moving
-// touchend = when finger lifts the screen
-// touchenter = enters the element
-// touchleave = when finger leaves the element
-// touchcancel = when finger dissapears from scree
+////////////////////////////////////////////
+//// Script Loading and Async Loading //////
+////////////////////////////////////////////
 
-// DOMContentLoaded is needed to ensure that DOM tree is buidt
+// Defer tells browser than he need to continue render HTML
+// and  load script in the background
+// script will only run when DOMtree is fully loaded
+// if 2 script files present they will load in order
 
-// (e) in eventListener("touch****") have addtitional properties
-// e.touches = shows how many fingers are currently used on all screen
-// e.targetTouches = shows how many finders used in THIS element
-// e.changedTouches = how many fingers are changed (had 3 on, 1 moved out of screen = 2)
+// <script defer src="script.js"></script>
+// <p>Hello World!</p>
+//  <p>Second Message</p>
 
-window.addEventListener("DOMContentLoaded", () => {
-  const box = document.querySelector(".box");
-  box.addEventListener("touchstart", (e) => {
-    e.preventDefault();
-    console.log("Start");
-    console.log(e.targetTouches);
-  });
-  box.addEventListener("touchmove", (e) => {
-    e.preventDefault();
-    console.log("Moving");
-  });
-  box.addEventListener("touchend", (e) => {
-    e.preventDefault();
-    console.log("ENd");
-  });
-});
+const p = document.querySelectorAll("p");
+
+// if defer not used console log will show 0 as <p> not created yet
+console.log(p); // NodeList []
+
+// page dosnt wait async scripts
+// async scripts dont wait each other, will load Asap
+
+// <script async src="script.js"></script>
+
+// functions to load scripts
+function loadScript(src) {
+  // can also create scripts with JS
+  const script = document.createElement("script");
+  //Dynamicly created scripts load like asinc by default
+  script.async = false;
+  script.src = "js/test.js";
+  document.body.append(script);
+}
+// because async = false they will load in order
+loadScript("js/test.js");
+loadScript("js/another.js");
