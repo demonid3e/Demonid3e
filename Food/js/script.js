1019492c22c0382c35c7412d281a2b3f705f16bb
demonid3e <71412024+demonid3e@users.noreply.github.com>
2021-10-11 01:40:00 +0100

    Started on food website

---
+++ Food/js/script.js
@@ -0,0 +1,41 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", () => {
+  const tabs = document.querySelectorAll(".tabheader__item"),
+    tabsContent = document.querySelectorAll(".tabcontent"),
+    tabsParent = document.querySelector(".tabheader__items");
+
+  function hideTabContent() {
+    tabsContent.forEach((item) => {
+      item.style.display = "none";
+    });
+    tabs.forEach((item) => {
+      item.classList.remove("tabheader__item_active");
+    });
+  }
+
+  // if function was called without arguments the default will be 0
+  function showTabContent(i = 1) {
+    tabsContent[i].style.display = "block";
+
+    // dont add . because classList already knows its class
+    tabs[i].classList.add("tabheader__item_active");
+  }
+
+  hideTabContent();
+  showTabContent();
+
+  tabsParent.addEventListener("click", (event) => {
+    const target = event.target;
+
+    if (target && target.classList.contains("tabheader__item")) {
+      tabs.forEach((item, i) => {
+        if (target == item) {
+          console.log(item, i);
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
+});
--- Learning Course Demon/script/compendium.js
+++ Learning Course Demon/script/compendium.js
@@ -1,5 +1,86 @@
 "use strict";
 
+///////////////////////////////////////
+////// Class Lists ////////////////////
+///////////////////////////////////////
+
+//  <div id="first" class="btn-block">
+//  <button class="blue some"></button>
+//  <button></button>
+//  <button></button>
+//  </div>
+const btn = document.querySelectorAll("button");
+
+btn.forEach((e) => {
+  console.log(e.classList.length);
+});
+
+// classList.item will show first class (blue)
+console.log(btn[0].classList.item(0));
+
+// will add classlist red ot button 3
+console.log(btn[2].classList.add("red"));
+// remove class blue
+console.log(btn[2].classList.remove("blue"));
+// will switch on class if its not there or turn off if it is there
+// will not work as good in complex scripts??
+console.log(btn[2].classList.toggle("red"));
+
+// returns Boolean value
+if (btn[1].classList.contains("red")) {
+  console.log("red");
+}
+
+// will check if it doesnt have class red
+btn[0].addEventListener("click", () => {
+  if (!btn[1].classList.contains("red")) {
+    btn[1].classList.add("red");
+  } else {
+    btn[1].classList.remove("red");
+  }
+});
+
+// Old style. Will return all classes of the item
+console.log(btn[0].className);
+
+////////////////////////////////
+////////Event delegation////////
+////////////////////////////////
+
+const btnBlock = document.querySelector(".btn-block");
+
+btnBlock.addEventListener("click", (event) => {
+  if (event.target && event.target.tagName == "BUTTON") {
+    console.log("Hello");
+  }
+});
+
+btnBlock.addEventListener("click", (event) => {
+  if (event.target && event.target.classList.contains("red")) {
+    console.log("bye");
+  }
+});
+
+const newButton = document.createElement("button");
+newButton.classList.add("green");
+btnBlock.append(newButton);
+
+// newButton wont say "test" with forEach
+btn.forEach((e) => {
+  e.addEventListener("click", () => {
+    console.log("test");
+  });
+});
+
+// event.target.matches(); more advances form.
+// in this case uses 2 selector button and red
+
+btnBlock.addEventListener("click", (event) => {
+  if (event.target && event.target.matches("button.red")) {
+    console.log("TOST");
+  }
+});
+
 ////////////////////////////////////////////
 //// Script Loading and Async Loading //////
 ////////////////////////////////////////////
