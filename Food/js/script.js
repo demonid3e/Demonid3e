fa2bd5f7d5e996356de2d5f12f3a7dfdeed80e19
demonid3e <71412024+demonid3e@users.noreply.github.com>
2021-12-09 01:12:21 +0000

    rewrote method to POST and GET using json server and db.json

--- Food/js/script.js
+++ Food/js/script.js
@@ -237,50 +237,95 @@ window.addEventListener("DOMContentLoaded", () => {
       this.parent.append(element);
     }
   }
+
+  const getResorces = async (url) => {
+    // await will wait for any responce
+    const res = await fetch(url);
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
+    return await res.json();
+  };
+
+  getResorces("http://localhost:3000/menu").then((data) => {
+    // using destructurisation of data getting out values of fetch objects
+    data.forEach(({ img, altimg, title, descr, price }) => {
+      // inserting values as arguments to new Menucard
+      new MenuCard(
+        img,
+        altimg,
+        title,
+        descr,
+        price,
+        ".menu .container"
+      ).render();
+    });
+  });
+
+  // another method to create Menus withour using classes and patterns
+  // getResorces("http://localhost:3000/menu").then((data) => createCard(data));
+
+  // function createCard(data) {
+  //   data.forEach(({ img, altimg, title, descr, price }) => {
+  //     const element = document.createElement("div");
+  //     element.classList.add("menu__item");
+  //     element.innerHTML = `
+  //         <img src=${img} alt=${altimg} />
+  //         <h3 class="menu__item-subtitle">${title}</h3>
+  //         <div class="menu__item-descr">${descr}</div>
+  //         <div class="menu__item-divider"></div>
+  //         <div class="menu__item-price">
+  //         <div class="menu__item-cost">Цена:</div>
+  //         <div class="menu__item-total"><span>${price}</span> грн/день</div>
+  //         </div>`;
+  //     document.querySelector(".menu .container").append(element);
+  //   });
+  // }
+
   // need send data in "";
   // need to combine "" with `` or ''
 
   // this syntaxis creates element like unknown function it cant be accessed later
   //only exist to run
-  new MenuCard(
-    "img/tabs/vegy.jpg",
-    "vegy",
-    'Меню "Фитнес"',
-    `Меню "Фитнес" - это новый подход к приготовлению блюд: больше
-    свежих овощей и фруктов. Продукт активных и здоровых людей. Это
-    абсолютно новый продукт с оптимальной ценой и высоким качеством!`,
-    // get number not string
-    5,
-    // in class menu class container
-    `.menu .container`
-  ).render();
+  // new MenuCard(
+  //   "img/tabs/vegy.jpg",
+  //   "vegy",
+  //   'Меню "Фитнес"',
+  //   `Меню "Фитнес" - это новый подход к приготовлению блюд: больше
+  //   свежих овощей и фруктов. Продукт активных и здоровых людей. Это
+  //   абсолютно новый продукт с оптимальной ценой и высоким качеством!`,
+  //   // get number not string
+  //   5,
+  //   // in class menu class container
+  //   `.menu .container`
+  // ).render();
 
-  new MenuCard(
-    "img/tabs/elite.jpg",
-    "elite",
-    "Меню “Премиум”",
-    `В меню “Премиум” мы используем не только красивый дизайн упаковки,
-    но и качественное исполнение блюд. Красная рыба, морепродукты,
-    фрукты - ресторанное меню без похода в ресторан!`,
-    // get number not string
-    9,
-    // in class menu class container
-    `.menu .container`
-  ).render();
+  // new MenuCard(
+  //   "img/tabs/elite.jpg",
+  //   "elite",
+  //   "Меню “Премиум”",
+  //   `В меню “Премиум” мы используем не только красивый дизайн упаковки,
+  //   но и качественное исполнение блюд. Красная рыба, морепродукты,
+  //   фрукты - ресторанное меню без похода в ресторан!`,
+  //   // get number not string
+  //   9,
+  //   // in class menu class container
+  //   `.menu .container`
+  // ).render();
 
-  new MenuCard(
-    "img/tabs/post.jpg",
-    "post",
-    'Меню "Постное"',
-    `Меню “Постное” - это тщательный подбор ингредиентов: полное
-    отсутствие продуктов животного происхождения, молоко из миндаля,
-    овса, кокоса или гречки, правильное количество белков за счет тофу
-    и импортных вегетарианских стейков.`,
-    // get number not string
-    13,
-    // in class menu class container
-    `.menu .container`
-  ).render();
+  // new MenuCard(
+  //   "img/tabs/post.jpg",
+  //   "post",
+  //   'Меню "Постное"',
+  //   `Меню “Постное” - это тщательный подбор ингредиентов: полное
+  //   отсутствие продуктов животного происхождения, молоко из миндаля,
+  //   овса, кокоса или гречки, правильное количество белков за счет тофу
+  //   и импортных вегетарианских стейков.`,
+  //   // get number not string
+  //   13,
+  //   // in class menu class container
+  //   `.menu .container`
+  // ).render();
 
   const forms = document.querySelectorAll("form");
 
@@ -291,10 +336,23 @@ window.addEventListener("DOMContentLoaded", () => {
   };
 
   forms.forEach((item) => {
-    postData(item);
+    bindPostData(item);
   });
 
-  function postData(form) {
+  // use async to wait for promise to resolve, must be used with await
+  const postData = async (url, data) => {
+    // await will wait for any responce
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: data,
+    });
+    return await res.json();
+  };
+
+  function bindPostData(form) {
     form.addEventListener("submit", (e) => {
       e.preventDefault();
 
@@ -312,10 +370,14 @@ window.addEventListener("DOMContentLoaded", () => {
 
       const formData = new FormData(form);
       // convert form data for json
-      const object = {};
-      formData.forEach(function (value, key) {
-        object[key] = value;
-      });
+      // const object = {};
+      // formData.forEach(function (value, key) {
+      //   object[key] = value;
+      // });
+
+      // instead of ^, create arrays of arrays, then creates object from arrays
+      // the creates JSON format using stringify
+      const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
       // request.send(json);
       // request.send(formData);
@@ -324,14 +386,7 @@ window.addEventListener("DOMContentLoaded", () => {
       // request.open("POST", "server.php");
 
       // fetch will not throw "catch" if http request failed
-      fetch("server.php", {
-        method: "POST",
-        headers: {
-          "Content-type": "application/json",
-        },
-        body: JSON.stringify(object),
-      })
-        .then((response) => response.text())
+      postData("http://localhost:3000/requests", json)
         .then((data) => {
           console.log(data);
           showThanksModal(message.success);
@@ -383,8 +438,8 @@ window.addEventListener("DOMContentLoaded", () => {
     }, 4000);
   }
   fetch("http://localhost:3000/menu")
-  .then(data => data.json())
-  .then(res => console.log(res));
+    .then((data) => data.json())
+    .then((res) => console.log(res));
 });
 
-// to run server: npx json-server db.json 
+// to run server: npx json-server db.json
