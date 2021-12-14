160f040074c2676fd15b2f9cac228b29d715a6a8
demonid3e <71412024+demonid3e@users.noreply.github.com>
2021-12-14 14:59:43 +0000

    moved to shortnotes info about localstorage

--- shortnotes.js
+++ shortnotes.js
@@ -2,6 +2,60 @@
 "use strict";
 
 
+
+//////////////////////////////////
+//////// Local Storage ///////////
+//////////////////////////////////
+
+// only works within one domain
+// max storage space 5mb
+
+// localStorage.setItem("number", 5);
+
+// console.log(localStorage.getItem("number"));
+
+// localStorage.removeItem("test");
+
+// localStorage.clear();
+
+const checkbox = document.querySelector("#checkbox"),
+    newForm = document.querySelector("form"),
+    changeColor = document.querySelector("#color");
+
+
+if (localStorage.getItem("isChecked")) {
+    checkbox.checked = true;
+}
+if(localStorage.getItem("bg") === "changed"){
+    newForm.style.backgroundColor = "red";
+    }
+
+
+checkbox.addEventListener("change", () =>{
+localStorage.setItem("isChecked", true);
+});
+
+changeColor.addEventListener("click", () =>{
+if(localStorage.getItem("bg") === "changed"){
+localStorage.removeItem("bg");
+newForm.style.backgroundColor = "#fff";
+}else{
+    localStorage.setItem("bg", "changed");
+   newForm.style.backgroundColor = "red";
+}
+});
+
+const personnne = {
+    name: "Alex",
+    age: 25
+};
+
+const serializedPersonne = JSON.stringify(personnne);
+localStorage.setItem("alex", serializedPersonne);
+
+console.log(JSON.parse(localStorage.getItem("alex")));
+
+
 ///////////////////////////////////////////
 //////////////  ARRAYS ////////////////////
 //////////////////////////////////////////
