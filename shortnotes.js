a690b780a705eb801c1a059b21ebff6591f04315
demonid3e <71412024+demonid3e@users.noreply.github.com>
2021-12-20 01:26:42 +0000

    added example of selfcalling annonimous functions

--- shortnotes.js
+++ shortnotes.js
@@ -1,5 +1,36 @@
 "use strict";
 
+///// Modules //////
+
+const app = "123";
+
+const number = 1;
+
+// annonymous, self calling function expression
+(function () {
+  let number = 2;
+  console.log(number);
+  console.log(number + 3);
+})();
+
+console.log(number);
+
+/////
+// before usserr cant access privat function
+// returning result of privat function as sayHello
+// now we can use this method in usserr
+
+const usserr = (function () {
+  const privat = function () {
+    console.log("Its private");
+  };
+  return {
+    sayHello: privat,
+  };
+})();
+
+usserr.sayHello();
+
 ////////// INCAPSULATION ///////////
 
 // let you hide properties of object from user to interact with
