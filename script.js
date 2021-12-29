dfe5abeb939ae6f59c4d48ea20359f5acefb99b2
demonid3e <71412024+demonid3e@users.noreply.github.com>
2021-12-29 01:10:30 +0000

    added notes about libraries and frameworks

--- script.js
+++ script.js
@@ -1,39 +1,42 @@
 "use strict";
 
-// when sees try/catch, try executed first, catch ignored
-// if "try" has any error, catch will execute, and all other code 
-// after try/catch will execute too.
-// if this construction is not used then code will trow a error and will stop
+////////////////////////////////////
+////////// FRAMEWORKS //////////////
+////////////////////////////////////
 
-try {
-    console.log("Normal"); // will show normal
-    console.log(a);        // here is the error
-    console.log("result"); // this code is not executed due to error
-} catch (error) {
-    console.log("Error"); // this is executed due to error
-    console.log(error);
-    console.log(error.name);
-    console.log(error.message);
-    console.log(error.stack);
-} finally {
-    console.log("This code will execute regardless");
-}
+//          Angular
+// framework based on typescript
+// most difficult
+// what needed:
+// Node.js
+// Typescript
+// Webpack
+// MVC pattern
+// Angular
 
-console.log("still normal"); // this code is executed too
 
-// if same script is used on different pages where some elements dont exist:
-// Example: two pages index.html and contact.html
-// index.html has button/class active, contact.html doesnt
-// if try/catch contructed wasnt used then in index.html it will work fine
-// on contact html it will stop and the code after catch wouldnt execute 
-// if try/catch used then, it would check if that element exists
+//          React
+// library
+// what needed:
+// JSX pre processor
+// Babel
+// Webpack
+// React
+// Node.js
 
-try{
-    document.querySelector(".active").addEventListener("click", () =>{
-        console.log("click");
-    });
-} catch (e) {
-    console.log("button doesnt exist");
-}
 
-console.log("Normal");
+
+
+//          VUE.js
+// framework
+// Nativ JS
+// Webpack
+// Note.js
+// Vue
+
+
+// jQuery
+// native js
+// watch youtube jquery
+
+
