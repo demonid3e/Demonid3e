148f9ddee5fa35f7d9c8f2cbc609db3c678571b2
demonid3e <71412024+demonid3e@users.noreply.github.com>
2022-01-03 19:55:29 +0000

    added Jquery examples

--- Other/jquery/script.js
+++ Other/jquery/script.js
@@ -1,4 +1,28 @@
+"use strict";
+
+// importing $ function from jquery
 import $ from "jquery";
 
-const btn = $("#btn");
+const btn = $("#btn"); // like querryselector
+
+
+console.log(btn);
+
+// document content loaded
+$(document).ready(function () {
+
+// get first button, and on hover toggles class active
+    $(".list-item:first").hover(function (){
+        $(this).toggleClass("active");
+    });
+    $(".list-item:eq(2)").on("click", function () {
+        $(".image:even").fadeToggle();
+    });
+    $(".list-item:eq(4)").on("click", function () {
+        $(".image:odd").animate({
+            opacity: "toggle",
+            height: "toggle"
+        }, 3000);
+    });
+});
 
