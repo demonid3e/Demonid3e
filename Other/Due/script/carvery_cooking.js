18fb49d681a2143089a8ba22504ad1fef9e6bc41
demonid3e <71412024+demonid3e@users.noreply.github.com>
2022-01-21 15:42:07 +0000

    working on due

--- Other/Due/script/carvery_cooking.js
+++ Other/Due/script/carvery_cooking.js
@@ -3,16 +3,26 @@
 import exportButtons from "./modules/buttons.js";
 import exportHeaderButtons from "./modules/header.js";
 
-const selectCooking = document.querySelector("#carvery_cooking");
+const selectCooking = document.querySelector("#carvery_cooking"),
+    blankData = document.querySelector('[data-value="blank"]'),
+    cookingWrapper = document.querySelector(".cooking_wrapper");
+
+
+    cookingWrapper.style.display = "none";
 
 selectCooking.addEventListener("change", () => {
-    console.log("Changed");
+    if(blankData.value != "blank"){
+        cookingWrapper.style.display = "block";
+    }
 })
 
+console.log("this is blank value", blankData.value);
 
 // create a layout with ${} in object and make it display none,
 // if blank display none, if one of the selection 
 // display true and all values are corresponding to selection
+
+
 
 exportButtons(4);
 exportHeaderButtons();
