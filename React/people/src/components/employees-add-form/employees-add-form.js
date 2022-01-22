58bab6e972a83b5e875e0ed3e51abacf4bea0345
demonid3e <71412024+demonid3e@users.noreply.github.com>
2022-01-22 22:21:55 +0000

    react commit

--- React/people/src/components/employees-add-form/employees-add-form.js
+++ React/people/src/components/employees-add-form/employees-add-form.js
@@ -1,29 +1,43 @@
 import "./employees-add-form.css";
+import {Component} from "react";
 
 
-// using function here instead of class to render form component
-const EmployeesAddForm = () => {
-  return (
-    <div className="app-add-form">
-      <h3>Add new employee</h3>
-      <form className="add-form d-flex">
-        <input
-          type="text"
-          className="form-control new-post-label"
-          placeholder="What`s his name?"
-        />
-        <input
-          type="number"
-          className="form-control new-post-label"
-          placeholder="Wage in $?"
-        />
 
-        <button type="submit" className="btn btn-outline-light">
-          Add
-        </button>
-      </form>
-    </div>
-  );
-};
+class EmployeesAddForm extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      name: "",
+      salary: ""
+
+    }
+  }
+  render(){
+
+      return (
+      <div className="app-add-form">
+        <h3>Add new employee</h3>
+        <form className="add-form d-flex">
+          <input
+            type="text"
+            className="form-control new-post-label"
+            placeholder="What`s his name?"
+          />
+          <input
+            type="number"
+            className="form-control new-post-label"
+            placeholder="Wage in $?"
+          />
+  
+          <button type="submit" className="btn btn-outline-light">
+            Add
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+  
+
 
 export default EmployeesAddForm;
