ca96e087717bba5bd5ee07670a476770c42c3e51
demonid3e <71412024+demonid3e@users.noreply.github.com>
2022-01-28 17:28:09 +0000

    created CSS files for Due and added Bootstrap to all html Due

--- Other/Due/script/modules/header.js
+++ Other/Due/script/modules/header.js
@@ -2,7 +2,17 @@
 // header buttons
 
 function exportHeaderButtons (){
-    const backBtn = document.querySelector('[data-button="back"]');
+    const backBtn = document.querySelector('[data-button="back"]'),
+          deleteBtn = document.querySelector('[data-button="delete"]'),
+          sendBtn = document.querySelector('[data-button="send"]'),
+          selectBtn = document.querySelector('[data-button="select"]'),
+          btn = document.querySelectorAll("button");
+    
+    btn.forEach ((e) =>{
+      e.classList.add("btn", "btn-sm");
+      console.log(e);
+    });
+
     backBtn.addEventListener("click", () =>{
       window.location.href="index.html";
       console.log("headder back called");
