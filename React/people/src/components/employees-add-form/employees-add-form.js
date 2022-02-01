1fee517c5fbac276b145232a140826f8f206c9df
demonid3e <71412024+demonid3e@users.noreply.github.com>
2022-02-01 13:52:00 +0000

    React/people fixed bug where you could add empty values for person in form

--- React/people/src/components/employees-add-form/employees-add-form.js
+++ React/people/src/components/employees-add-form/employees-add-form.js
@@ -20,9 +20,18 @@ class EmployeesAddForm extends Component {
   // this.state.salary as salary argument
   onSubmit = (e) => {
     e.preventDefault();
-    this.props.onAdd(this.state.name, this.state.salary);
-    // this line sets empty strings after add
-    this.setState({ name: "", salary: "" });
+    console.log(this.state.name.length);
+    if(this.state.name.length >= 3 && this.state.salary.length >= 1){
+      this.props.onAdd(this.state.name, this.state.salary);
+      // this line sets empty strings after add
+      this.setState({ name: "", salary: "" });
+      console.log("all good")
+    }else if (this.state.name.length < 3){
+      console.log("Name is too short, please enter longer name");
+    }else if (this.state.salary.length <= 0){
+      console.log("Salary cant be 0, please enter correct salary");
+    }
+
   };
 
   render() {
