9512b134cecb0c68934e055104505e89c5183068
demonid3e <71412024+demonid3e@users.noreply.github.com>
2022-02-09 19:31:27 +0000

    added set date function to Due/Carvery sides

--- Other/Due/script/carvery_sides.js
+++ Other/Due/script/carvery_sides.js
@@ -2,9 +2,15 @@
 
 import exportButtons from "./modules/buttons.js";
 import exportHeaderButtons from "./modules/header.js";
+import setCurrentTime from "./modules/setTime.js"
+
+const setDateOne = document.querySelector("#side_date_time"),
+    setDateTwo = document.querySelector("#side_date_time_2");
 
 
 
 exportHeaderButtons();
 exportButtons(3);
+setCurrentTime(setDateOne);
+setCurrentTime(setDateTwo);
 
