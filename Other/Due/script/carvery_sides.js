b2ffa21679082854ec0f7a5de1b402f2c78af624
demonid3e <71412024+demonid3e@users.noreply.github.com>
2022-02-11 21:20:07 +0000

    need to fix post request due sides

--- Other/Due/script/carvery_sides.js
+++ Other/Due/script/carvery_sides.js
@@ -9,6 +9,46 @@ const setDateOne = document.querySelector("#side_date_time"),
 
 
 
+
+function getFormData() {
+    const carverySides = {
+          title: document.querySelector("title").innerText,
+          product: document.querySelector(".carvery_sides_product").value,
+          cookTemp: document.querySelector(".carvery_sides_cook_temp").value,
+          timeStart: setDateOne.value,
+          cookTempTwoHours: document.querySelector("#sides_2hour_temp").value,
+          timeTwoHours: setDateTwo.value,
+          decision: document.querySelector("#sides_select").value       
+        }
+        console.log(carverySides);
+        return carverySides
+      }
+
+
+const selectBtn = document.querySelector(".carvery_mains_select ");
+
+selectBtn.addEventListener("click", () => sendData());
+        
+    async function postData(url = "") {
+      getFormData();
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({carverySides}),
+      });
+      return response.json();
+    }
+    function sendData() {
+      postData("http://localhost:3000/post").then((data) => {
+        console.log(data); // JSON data parsed by `data.json()` call
+      });
+    }
+
+    
+
+
 exportHeaderButtons();
 exportButtons(3);
 setCurrentTime(setDateOne);
