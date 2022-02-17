4bda43cf0448b725bde7fa41f7066d75f33157de
demonid3e <71412024+demonid3e@users.noreply.github.com>
2022-02-17 02:35:16 +0000

    mern Added client side and added routes

---
+++ mern/client/src/pages/LinksPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route } from "react-router-dom";
+
+export const LinksPage = () =>{
+    return(
+        <Route>
+        <div>
+            <h1>Links Page</h1>
+        </div>
+        </Route>
+    )
+ 
+}
