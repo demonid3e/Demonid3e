4c303386a4846692d0f8df67e5647ccae11974c3
demonid3e <71412024+demonid3e@users.noreply.github.com>
2022-02-18 20:37:42 +0000

    Due Completed cooking page with fetch

--- Other/Due/script/carvery_cooking.js
+++ Other/Due/script/carvery_cooking.js
@@ -10,23 +10,48 @@ const selectCooking = document.querySelector("#carvery_cooking"),
     cookingSessionName = document.querySelector(".cooking_session_name"),
     cookingNonBreakfast = document.querySelector(".cooking_non_breakfast"),
     sendBtn = document.querySelector(".carvery_mains_send"),
+    selectBtn = document.querySelector(".carvery_mains_select"),
     burgers = document.querySelectorAll(".burger");
 
+var dataFields = [];
+var CookingData =  {};
 
 
 
 // event listeners:
 sendBtn.addEventListener("click", () => sendData());
+selectBtn.addEventListener("click", () => checkCookingStatus());
 // Session selector
 selectCooking.addEventListener("change", () => checkCookingStatus());
 
 
 
 function checkCookingStatus () {
+const  firstName = document.querySelector("#first_product"),
+  firstInputLeft = document.querySelector("#first_temperature_left"),
+  firstInputRight = document.querySelector("#first_temperature_right"),
+  secondName = document.querySelector("#second_product"),
+  secondInputLeft = document.querySelector("#second_temperature_left"),
+  secondInputRight = document.querySelector("#second_temperature_right"),
+  thirdName = document.querySelector("#third_product"),
+  thirdInputLeft = document.querySelector("#third_temperature_left"),
+  thirdInputRight = document.querySelector("#third_temperature_right"),
+  burgerOneLeft = document.querySelector("#burger_one_left"),
+  burgerOneRight = document.querySelector("#burger_one_right"),
+  burgerTwoLeft = document.querySelector("#burger_two_left"),
+  burgerTwoRight = document.querySelector("#burger_two_right"),
+  burgerThreeLeft = document.querySelector("#burger_three_left"),
+  burgerThreeRight = document.querySelector("#burger_three_right");
+
+  dataFields = [firstName, firstInputLeft, firstInputRight, secondInputLeft, secondInputRight, secondName, thirdInputLeft,thirdInputRight, thirdName, burgerOneLeft, burgerOneRight, burgerTwoLeft, burgerTwoRight, burgerThreeLeft, burgerThreeRight];
+
     if(selectCooking.value === "Blank"){
         cookingWrapper.classList.add("hide");
         cookingNonBreakfast.classList.add("hide");
         cookingSessionName.innerHTML = "";
+        CookingData = {};
+        console.log(CookingData);
+
     }
     if(selectCooking.value === "Lunch" || selectCooking.value === "Evening"){
         cookingWrapper.classList.remove("hide");
@@ -34,30 +59,52 @@ function checkCookingStatus () {
         cookingSessionName.innerHTML = selectCooking.value;
         burgers.forEach((item, i) =>{
             item.innerHTML = `${selectCooking.value} Burger ${i + 1}`;
-        });    
+        });
+        CookingData[selectCooking.value] = {         
+            firstRow: `${firstName.value} ${firstInputLeft.value} ${firstInputRight.value}`,
+            secondRow: `${secondName.value} ${secondInputLeft.value} ${secondInputRight.value}`,
+            thirdRow: `${thirdName.value} ${thirdInputLeft.value} ${thirdInputRight.value}`,
+            LunchBurger1: `${burgerOneLeft.value} ${burgerOneRight.value}`,
+            LunchBurger2: `${burgerTwoLeft.value} ${burgerTwoRight.value}`,
+            LunchBurger3: `${burgerThreeLeft.value} ${burgerThreeRight.value}`    
+        } 
+
     }
     if(selectCooking.value === "Breakfast"){
         cookingNonBreakfast.classList.add("hide");
         cookingWrapper.classList.remove("hide");
         cookingSessionName.innerHTML = selectCooking.value;
+        CookingData =  {
+          breakfast: {
+            firstRow: `${firstName.value} ${firstInputLeft.value} ${firstInputRight.value}`,
+            secondRow: `${secondName.value} ${secondInputLeft.value} ${secondInputRight.value}`
+          }
+        }
     }
+    console.log(CookingData);
+    return CookingData, dataFields;
 }
 
-function formData () {
-    
+async function dataFormClear (){
+  dataFields.forEach( (item) =>{
+    item.value = "";
+  })
+  
 }
 
 
 // fetch that  prepeares  carverySides data for json file
 async function postData(url = "") {
+  checkCookingStatus();
+  dataFormClear();
   const response = await fetch(url, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ "test": "asdasd" }),
+    body: JSON.stringify({ CookingData }),
   });
-
+  
   return response.json();
 }
 
