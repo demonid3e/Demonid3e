d48e5a04cf8128100dabf85e06bb8750bde0da30
demonid3e <71412024+demonid3e@users.noreply.github.com>
2022-02-20 12:44:43 +0000

    Due added comment function

--- Other/Due/script/carvery_comments.js
+++ Other/Due/script/carvery_comments.js
@@ -4,7 +4,21 @@ import exportButtons from "./modules/buttons.js";
 import exportHeaderButtons from "./modules/header.js";
 
 
+const commentWrapper = document.querySelector(".comment_wrapper");
+const selectButton = document.querySelector(".carvery_mains_select");
 
+var commentData = {};
+
+selectButton.addEventListener("click", () => getCommentData());
+
+function getCommentData () {
+    let elements = commentWrapper.elements;
+    for(let i = 0; i < elements.length; i++){
+        let item = elements.item(i);
+        commentData[item.name] = item.value;
+    }
+    console.log(commentData);
+}
 
 
 
