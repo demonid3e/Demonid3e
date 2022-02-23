dc9b5435c79b6cd5ad30a224eb33330863187d00
demonid3e <71412024+demonid3e@users.noreply.github.com>
2022-02-23 14:11:00 +0000

    Due added sendData function to comment page

--- Other/Due/script/carvery_comments.js
+++ Other/Due/script/carvery_comments.js
@@ -6,10 +6,12 @@ import exportHeaderButtons from "./modules/header.js";
 
 const commentWrapper = document.querySelector(".comment_wrapper");
 const selectButton = document.querySelector(".carvery_mains_select");
+const sendButton = document.querySelector(".carvery_mains_send");
 
 var commentData = {};
 
 selectButton.addEventListener("click", () => getCommentData());
+sendButton.addEventListener("click", () => sendData());
 
 function getCommentData () {
     let elements = commentWrapper.elements;
@@ -20,7 +22,26 @@ function getCommentData () {
     console.log(commentData);
 }
 
-
+// fetch that  prepeares  carverySides data for json file
+async function postData(url = "") {
+    getCommentData();
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ commentData }),
+    });
+    commentWrapper.reset();
+    return response.json();
+  }
+  
+  // fetch that sends data to server
+  function sendData() {
+    postData("http://localhost:3000/Comments").then((data) => {
+      console.log(data); // JSON data parsed by `data.json()` call
+    });
+  }
 
 
 
