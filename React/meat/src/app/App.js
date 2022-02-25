e547fd8a8c7dd9380884ae1694aef9320f9eea1c
demonid3e <71412024+demonid3e@users.noreply.github.com>
2022-02-25 23:54:43 +0000

    meat

--- React/meat/src/app/App.js
+++ React/meat/src/app/App.js
@@ -2,19 +2,33 @@ import MeatPanel from "../components/meat-panel/meat-panel";
 import MeatInput from "../components/meat-input/meat-input";
 import MeatDelivery from "../components/meat-delivery/meat-delivery";
 import MeatDay from "../components/meat-day/meat-day";
+import {Component} from "react";
 
 
 
 
-function App() {
-  return (
-    <div className="App">
-      <MeatPanel/>
-      <MeatInput/>
-      <MeatDelivery/>
-      <MeatDay />
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      beef: 0
+    }
+    onUpdate = (beef) => {
+      this.setState({ beef });
+    };
+
+  }
+  render() {
+    return (
+      <div className="App">
+        <MeatPanel/>
+        <MeatInput onUpdate={this.onUpdate}/>
+        <MeatDelivery/>
+        <MeatDay />
+      </div>
+    );
+  }
+
 }
 
 export default App;
--- React/meat/src/components/meat-input/meat-input.js
+++ React/meat/src/components/meat-input/meat-input.js
@@ -5,23 +5,33 @@ class MeatInput extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            
-
+        beef: 0,
+        gammon: 0,
+        turkey: 0,
+        pork: 0
         }
+        
     }
+    onUpdate = (e) => {
+        const beef = e.target.value;
+        this.setState({ beef });
+        this.props.onUpdate(beef);
+        console.log(this.state);
+      };
     render () {
+        const {beef, gammon, turkey, pork} = this.state;
         return (
             <div className="meat-input">
                 <h1>Please Enter how many Joints you have in stock:</h1>
                 <form className="meat-input-form">
                     <label htmlFor="input-beef"> Beef: </label>
-                    <input type="number" id="input-beef" className="input" placeholder="0" step="0.01"/>
+                    <input type="number" id="input-beef" value={beef} onChange={this.onUpdate} className="input"/>
                     <label htmlFor="input-gammon"> Gammon: </label>
-                    <input type="number" id="input-gammon" className="input" placeholder="0" step="0.01"/>
+                    <input type="number" id="input-gammon" value={gammon} onChange={this.onUpdate} className="input"   step="0.01"/>
                     <label htmlFor="input-turkey"> Turkey: </label>        
-                    <input type="number" id="input-turkey" className="input"  placeholder="0" step="0.01"/>
+                    <input type="number" id="input-turkey" value={turkey} onChange={this.onUpdate} className="input"    step="0.01"/>
                     <label htmlFor="input-pork"> Pork: </label>
-                    <input type="number" id="input-pork" className="input" placeholder="0" step="0.01" />
+                    <input type="number" id="input-pork" value={pork} onChange={this.onUpdate} className="input"   step="0.01" />
 
                 </form>
             </div>
