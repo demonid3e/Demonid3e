498e7b40a07e3c9acc65a35c241cdca076ad98e5
demonid3e <71412024+demonid3e@users.noreply.github.com>
2022-02-26 02:01:31 +0000

    cant figure out data

--- React/meat/src/components/meat-input/meat-input.js
+++ React/meat/src/components/meat-input/meat-input.js
@@ -13,10 +13,8 @@ class MeatInput extends Component {
         
     }
     onUpdate = (e) => {
-        const beef = e.target.value;
-        this.setState({ beef });
-        this.props.onUpdate(beef);
-        console.log(this.state);
+        this.setState({ [e.target.name]: e.target.value });
+        console.log("meat input" , this.state);
       };
     render () {
         const {beef, gammon, turkey, pork} = this.state;
@@ -25,13 +23,13 @@ class MeatInput extends Component {
                 <h1>Please Enter how many Joints you have in stock:</h1>
                 <form className="meat-input-form">
                     <label htmlFor="input-beef"> Beef: </label>
-                    <input type="number" id="input-beef" value={beef} onChange={this.onUpdate} className="input"/>
+                    <input type="number" id="input-beef" name="beef" value={beef} onChange={this.onUpdate} className="input"/>
                     <label htmlFor="input-gammon"> Gammon: </label>
-                    <input type="number" id="input-gammon" value={gammon} onChange={this.onUpdate} className="input"   step="0.01"/>
+                    <input type="number" id="input-gammon" name="gammon" value={gammon} onChange={this.onUpdate} className="input"   step="0.01"/>
                     <label htmlFor="input-turkey"> Turkey: </label>        
-                    <input type="number" id="input-turkey" value={turkey} onChange={this.onUpdate} className="input"    step="0.01"/>
+                    <input type="number" id="input-turkey" name="turkey" value={turkey} onChange={this.onUpdate} className="input"    step="0.01"/>
                     <label htmlFor="input-pork"> Pork: </label>
-                    <input type="number" id="input-pork" value={pork} onChange={this.onUpdate} className="input"   step="0.01" />
+                    <input type="number" id="input-pork" name="pork" value={pork} onChange={this.onUpdate} className="input"   step="0.01" />
 
                 </form>
             </div>
