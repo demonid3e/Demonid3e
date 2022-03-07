6fa67475e82b2c32b19cd60a38e19f8043c246a0
demonid3e <71412024+demonid3e@users.noreply.github.com>
2022-03-07 17:50:15 +0000

    marvel added try it button and fixed empty image object-fit

--- marvel/src/components/randomChar/RandomChar.js
+++ marvel/src/components/randomChar/RandomChar.js
@@ -7,15 +7,25 @@ import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
 
 class RandomChar extends Component {
-    constructor (props) {
-        super(props);
-        this.updateChar();
-    }
+
     state = {
         char: {},
         loading: true,
-        error: false
+        error: false,
+        empty: true
     }
+
+    componentDidMount () {
+        this.updateChar();
+
+    }
+
+    updateRandomChar = () => {
+       this.updateChar();
+    }
+
+
+
         // using component creating a new class to test and passing responce into console
      marvelService = new MarvelService();
 
@@ -51,6 +61,7 @@ class RandomChar extends Component {
         const errorMessage = error ? <ErrorMessage/> : null;
         const spinner = loading ? <Spinner/> : null;
         const content = !(loading || error) ? <View char={char}/> : null;
+        
 
 
         return (
@@ -68,7 +79,7 @@ class RandomChar extends Component {
                         Or choose another one
                     </p>
                     <button className="button button__main">
-                        <div className="inner">try it</div>
+                        <div className="inner" onClick={this.updateRandomChar}>try it</div>
                     </button>
                     <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
                 </div>
@@ -82,10 +93,11 @@ class RandomChar extends Component {
 
 const View = ({char}) => {
     const {name, description, wiki, thumbnail, homepage} = char;
+    const isEmpty = ("http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg" === thumbnail);
 
     return (
         <div className="randomchar__block">
-        <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+        <img src={thumbnail} alt="Random character" style={isEmpty ? {objectFit: "contain"} : {objectFit: "cover"}}   className="randomchar__img"/>
         <div className="randomchar__info">
             <p className="randomchar__name">{name}</p>
             <p className="randomchar__descr">{description}</p>
