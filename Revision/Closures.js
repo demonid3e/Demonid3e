e5ab782d53a2f00a4f223310ffa8cb29d578b69e
demonid3e <71412024+demonid3e@users.noreply.github.com>
2022-04-05 19:37:41 +0100

    Closures

--- Revision/Closures.js
+++ Revision/Closures.js
@@ -1,38 +1,38 @@
-// let val = 7
-//   function createAdder() {
-//     function addNumbers(a, b) {
-//       let ret = a + b
-//       return ret
+// // let val = 7
+// //   function createAdder() {
+// //     function addNumbers(a, b) {
+// //       let ret = a + b
+// //       return ret
+// //     }
+// //     return addNumbers // = a+b
+// //   }
+// //   let adder = createAdder()
+// //  let sum = adder(val, 8)
+// //  console.log('example of function returning a function: ', sum)
+
+
+
+
+// function createCounter() {
+//     let counter = 0
+//     const myFunction = function() {
+//     counter = counter + 1
+//         return counter
 //     }
-//     return addNumbers // = a+b
-//   }
-//   let adder = createAdder()
-//  let sum = adder(val, 8)
-//  console.log('example of function returning a function: ', sum)
-
-
-
-
-function createCounter() {
-    let counter = 0
-    const myFunction = function() {
-    counter = counter + 1
-        return counter
-    }
-    return myFunction
-}
-    const increment = createCounter() // counter = 0
-    const c1 = increment() // counter = 1
-    const c2 = increment() // counter = 2
-    const c3 = increment() // counter = 3
-    console.log('example increment', c1, c2, c3)
+//     return myFunction
+// }
+//     const increment = createCounter() // counter = 0
+//     const c1 = increment() // counter = 1
+//     const c2 = increment() // counter = 2
+//     const c3 = increment() // counter = 3
+//     console.log('example increment', c1, c2, c3)
 
 
     
 
-// because "increment" is stored in variable, the value of counter is in the "increment",
-// so every time "increment" is called the function is loking in "closure" then,
-// in local scope and after that in global scope
+// // because "increment" is stored in variable, the value of counter is in the "increment",
+// // so every time "increment" is called the function is loking in "closure" then,
+// // in local scope and after that in global scope
 
 
 
@@ -40,18 +40,26 @@ function createCounter() {
 
 let c = 4
 function addX(x) {
+    console.log("this is x:" ,x);
   return function(n) {
+      console.log("this is n:", n , "this is x:", x);
      return n + x
   }
 }
-const addThree = addX(3)
-let d = addThree(c)
-console.log('example partial application', d)
 
-{
-let c = 4
-const addX = x => n => n + x
-const addThree = addX(3)
+const addThree = addX(3);
+console.log(addThree());
 let d = addThree(c)
-console.log('example partial application', d)
-}
+// console.log('example partial application', d)
+
+// {
+// let c = 4
+// const addX = x => n => n + x
+// const addThree = addX(3)
+// let d = addThree(c)
+// console.log('example partial application', d)
+// }
+
+
+
+console.log(3 + undefined);
