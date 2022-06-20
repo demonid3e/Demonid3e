dd5997278cd5c00f2b86f610ed1520b9b82b8043
demonid3e <71412024+demonid3e@users.noreply.github.com>
2022-06-20 23:21:00 +0100

    changed class components to use hooks

--- marvel/src/components/app/App.js
+++ marvel/src/components/app/App.js
@@ -2,23 +2,21 @@ import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
-import { Component } from "react";
+import { useState } from "react";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 import decoration from '../../resources/img/vision.png';
 
 
 
-class  App extends Component {
-    state = {
-        selectedChar: null
+const App = () => {
+
+    const [selectedChar, setChar] = useState(null);
+
+
+    const onCharSelected = (id) => {
+        setChar(id);
     }
 
-    onCharSelected = (id) => {
-        this.setState({
-            selectedChar: id
-        })
-    }
-render () {
     return (
             <div className="app">
                 <AppHeader/>
@@ -28,18 +26,18 @@ render () {
                     </ErrorBoundary>
                     <div className="char__content">
                         <ErrorBoundary>
-                            <CharList onCharSelected={this.onCharSelected}/>
+                            <CharList onCharSelected={onCharSelected}/>
                         </ErrorBoundary>
                         <ErrorBoundary>
-                            <CharInfo charId={this.state.selectedChar}/>
+                            <CharInfo charId={selectedChar}/>
                         </ErrorBoundary>
                     </div>
                     <img className="bg-decoration" src={decoration} alt="vision"/>
                 </main>
             </div>
         )
-    }
 }
+
 
 
 
