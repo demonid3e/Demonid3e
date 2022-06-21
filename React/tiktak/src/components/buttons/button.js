f50538bec4029b69a29539ca7a029f10b0e391fe
demonid3e <71412024+demonid3e@users.noreply.github.com>
2022-06-21 16:08:00 +0100

    tiktak added players option

--- React/tiktak/src/components/buttons/button.js
+++ React/tiktak/src/components/buttons/button.js
@@ -1,11 +1,27 @@
 import React from "react";
 import { Button } from "@mui/material";
+import { useState, useEffect } from "react";
+
 
 const PlayerButton = (props) => {
+
+    const [currentPlayer, setCurrentPlayer] = useState(null);
+
+
+    const onPlayerChange = (player) => {
+        setCurrentPlayer(player);
+    }
+
     return (
         <>
-            <Button style={style} variant = "contained">Player 1</Button>
-            <Button style={style} variant = "contained">Player 2</Button>
+            <div className="controls">
+                <Button style={style} variant = "contained">Start</Button>
+            </div>
+            <div className="players">
+                <div>Current player:{currentPlayer} </div>
+                <Button onClick={()  => onPlayerChange("Player 1")} style={style} variant = "contained">Player 1</Button>
+                <Button onClick={() => onPlayerChange("Player 2")} style={style} variant = "contained">Player 2</Button>
+            </div>
         </>
 
     
