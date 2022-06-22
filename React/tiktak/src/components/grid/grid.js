c1d33ab2ed3cb4b6a5ad831617acbea09b1bdf03
demonid3e <71412024+demonid3e@users.noreply.github.com>
2022-06-22 15:29:59 +0100

    tiktak not working

--- React/tiktak/src/components/grid/grid.js
+++ React/tiktak/src/components/grid/grid.js
@@ -2,50 +2,63 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Item from '../item-square/item-square';
+import { useState, useEffect } from 'react';
 
 const style = {
     margin: "5px",
 }
 
-export default function BasicGrid(props) {
+function BasicGrid(props) {
+
+    const [fieldValues, setFieldValues] = useState([]);
+
+    const onClick = (id) => {
+        const newArr = [...fieldValues];
+        setFieldValues(newArr[id] = "X");
+        console.log(newArr);
+    }
+
+    useEffect(() => console.log("useEffect"),[fieldValues]
+    );
+
   return (
       <>
-        <Box sx={{ flexGrow: 1 }}>
+        <Box  sx={{ flexGrow: 1 }}>
         <Grid container spacing={2}>
-            <Grid style={style} item xs={props.size}>
-            <Item current="X"></Item>
+            <Grid onClick={(e) => {onClick(e.currentTarget.id)}} id="0"  style={style} item xs={props.size}>
+            <Item  current={fieldValues[0]} key="d" ></Item>
             </Grid>
-            <Grid style={style} item xs={props.size}>
-            <Item current = "O"></Item>
+            <Grid onClick={(e) => {onClick(e.currentTarget.id)}} id="1" style={style} item xs={props.size}>
+            <Item current={fieldValues[1]} key={"g"} ></Item>
             </Grid>
-            <Grid style={style} item xs={props.size}>
-            <Item current = "X" ></Item>
+            <Grid onClick={(e) => {onClick(e.currentTarget.id)}} id="2" style={style} item xs={props.size}>
+            <Item current={fieldValues[2]} key={fieldValues[2]} ></Item>
             </Grid>
         </Grid>
         </Box>
         <Box sx={{ flexGrow: 1 }}>
         <Grid container spacing={2}>
-            <Grid style={style} item xs={props.size}>
-            <Item current="X"></Item>
+            <Grid onClick={(e) => {onClick(e.currentTarget.id)}} id="3" style={style} item xs={props.size}>
+            <Item current={fieldValues[3]} key={fieldValues[3]} ></Item>
             </Grid>
-            <Grid style={style} item xs={props.size}>
-            <Item current = "O"></Item>
+            <Grid onClick={(e) => {onClick(e.currentTarget.id)}} id="4" style={style} item xs={props.size}>
+            <Item current={fieldValues[4]} key={fieldValues[4]} ></Item>
             </Grid>
-            <Grid style={style} item xs={props.size}>
-            <Item current = "X" ></Item>
+            <Grid onClick={(e) => {onClick(e.currentTarget.id)}} id="5" style={style} item xs={props.size}>
+            <Item current={fieldValues[5]} key={fieldValues[5]} ></Item>
             </Grid>
         </Grid>
         </Box>    
         <Box sx={{ flexGrow: 1 }}>
         <Grid container spacing={2}>
-            <Grid style={style} item xs={props.size}>
-            <Item current="X"></Item>
+            <Grid onClick={(e) => {onClick(e.currentTarget.id)}} id="6" style={style} item xs={props.size}>
+            <Item current={fieldValues[6]} key={fieldValues[6]} ></Item>
             </Grid>
-            <Grid style={style} item xs={props.size}>
-            <Item current = "O"></Item>
+            <Grid onClick={(e) => {onClick(e.currentTarget.id)}} id="7" style={style} item xs={props.size}>
+            <Item current={fieldValues[7]} key={fieldValues[7]} ></Item>
             </Grid>
-            <Grid style={style} item xs={props.size}>
-            <Item current = "X" ></Item>
+            <Grid onClick={(e) => {onClick(e.currentTarget.id)}} id="8" style={style} item xs={props.size}>
+            <Item current={fieldValues[8]} key={fieldValues[8]}  ></Item>
             </Grid>
         </Grid>
         </Box>          
@@ -53,3 +66,5 @@ export default function BasicGrid(props) {
     
   );
 }
+
+export default BasicGrid;
