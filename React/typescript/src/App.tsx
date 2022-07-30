63f2f43a730a9821ddb4a7f209cf6eb587a346b3
demonid3e <71412024+demonid3e@users.noreply.github.com>
2022-07-30 11:13:48 +0100

    added new Typescript project

---
+++ React/typescript/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className='container'>
+          <h1 className='px-5, border'>Hello React!</h1>
+    </div>
+  );
+}
+
+export default App;
